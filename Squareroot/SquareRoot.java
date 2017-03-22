8317eb533bad7f8801e3db7deed5172d4cedcdd4
venky <vnandan.garigipati@gmail.com>
2017-03-22 14:36:46 +0530

    Squareroot with comments

--- Squareroot/SquareRoot.java
+++ Squareroot/SquareRoot.java
@@ -2,7 +2,11 @@ class SquareRoot{
 	static int squareRoot(int x){
 		int start = 1,end = x, ans = 0, mid = 0;
 		while(start <= end){
-			mid = (start + end) / 2;
+			/* Checking id the given number equal to 
+			 * square of the half to the given number
+			 * Evenry time making the number to it's half
+			*/
+			mid = (start + end) / 2;    
 			if(x ==0 || x==1)
 				return 1;
 			if(mid*mid == x)
